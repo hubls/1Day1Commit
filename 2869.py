c5dac4448f00626baa0ba10f57f37aeb0162da19
sangjun Lee <hubls@naver.com>
2022-09-08 14:52:17 +0900

    달팽이는 올라가고 싶다

---
+++ 2869.py
@@ -0,0 +1,33 @@
+a, b, v = input().split()
+a = int(a)
+b = int(b)
+v = int(v)
+
+day = 0
+sum = 0
+
+
+(v-b) / (a-b) 
+
+(v-b) % (a-b) != 0 
+
+if (v-b) % (a-b) == 0:
+    print(int((v-b) / (a-b)))
+elif (v-b) % (a-b) != 0 :
+    print(int((v-b) / (a-b)) + 1)
+
+
+
+
+'''
+while(1):
+    sum += a
+    day += 1
+    if (sum >=v):
+        break
+    else:
+        sum -=b
+
+print("day : ", day)
+
+'''
