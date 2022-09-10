f631265b62c930b2af96386c0b9a756e256c7c2e
sangjun Lee <hubls@naver.com>
2022-09-10 16:14:14 +0900

    큐

---
+++ 10845.py
@@ -0,0 +1,40 @@
+import sys
+
+n = int(input())
+
+
+
+queue = []
+for i in range(n):
+    s = sys.stdin.readline().split()
+
+    if s[0] == 'push':
+        queue.append(int(s[1]))
+
+    elif s[0] == 'pop' :
+        if len(queue) == 0 :
+            print(-1)
+        else:
+            print(queue.pop(0))
+        
+    elif s[0] == 'size':
+        print(len(queue))
+
+    elif s[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+
+    elif s[0] == 'front' :
+        if len(queue) == 0 :
+            print(-1)
+        else:
+            print(queue[0])
+
+    elif s[0] == 'back' :
+        if len(queue) == 0 :
+            print(-1)
+        else:
+            print(queue[-1])
+        
