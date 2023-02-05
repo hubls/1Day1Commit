f7cffde0c212372d66246322923bdcb553797b9c
leesangjun <hubls@naver.com>
2023-02-05 17:12:49 +0900

    최소 힙

---
+++ 1927.py
@@ -0,0 +1,17 @@
+import heapq
+import sys
+
+n = int(sys.stdin.readline())
+
+
+heap = []
+for i in range(n):
+    num = int(sys.stdin.readline())
+
+    if num == 0 :
+        if not heap:
+            print(0)
+        else:
+            print(heapq.heappop(heap))
+    else:
+        heapq.heappush(heap, num)
